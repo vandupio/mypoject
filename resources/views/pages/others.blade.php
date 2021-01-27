6eeb2d1092196b26f136a05a82ecfb3e65522712
vandupio <vandupio@gmail.com>
2021-01-27 09:32:29 +0800

    update others page

--- resources/views/pages/others.blade.php
+++ resources/views/pages/others.blade.php
@@ -9,13 +9,18 @@
 		</thead>
 		<tbody>
 			@foreach ($menus as $menu)
-			<tr>
-				<td>{{$menu->title}}</td>
-				<td><img src="{{ asset('storage/images/'.$menu->Imagefilename) }}" alt="{{$menu->Imagefilename}}" style="width: 100px;"></td>
-				<td>{{date_format($menu->created_at,'M d, Y')}}</td>
+			<tr style='cursor: pointer; cursor: hand;' onclick="window.location='/menu/{{$menu->feedid}}';">
+				<td style="padding:50px 0;">{{$menu->title}}</td>
+				<td><img src="{{ asset('storage/images/'.$menu->Imagefilename) }}" alt="{{$menu->Imagefilename}}" 
+					style="width: 200px;height: 100px;object-fit:cover;object-position: 20% 10%;"></td>
+
+				<td style="padding:50px 0;">{{date_format($menu->created_at,'M d, Y')}}</td>
+
 			</tr>
+		
 			@endforeach
 		</tbody>
 	</table>
 </div>
+
 @endsection
